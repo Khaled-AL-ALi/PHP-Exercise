a736eadc60931b0a1169596e729b6789596f6c3f
Khaled-AL-ALi <khaled2020pro@gmail.com>
2022-02-24 14:45:21 +0200

    step5

---
+++ php-basics-01/step5.php
@@ -0,0 +1,9 @@
+<?php
+$course_name="php";
+$enrolled_students=17;
+$price=23.5;
+$on_discount=true;
+
+echo "course name : {$course_name}\n Enrolled Students:{$course_name}\n Course price: {$price}$ USD \n Course on discount: {$on_discount}\n"
+
+?>
