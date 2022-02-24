e569235c79a3152540e39ca5e2a8e75b8705d5cd
Khaled-AL-ALi <khaled2020pro@gmail.com>
2022-02-24 15:21:19 +0200

    step8-a

---
+++ php-basics-01/step8-a.php
@@ -0,0 +1,6 @@
+<?php
+$arr=["Eggs", "Milk", "Cheese", "Water Pack", "Tissues", "Watermelon"];
+
+echo "Hello Sir, do you have {$arr[0]}, {$arr[1]}, and {$arr[2]}? Also if you sell fruits can I find a {$arr[5]}?";
+
+?>
