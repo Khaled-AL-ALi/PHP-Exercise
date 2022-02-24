ba2528fb91ec5c43666bfa00557d6ec5f55e5fc6
Khaled-AL-ALi <khaled2020pro@gmail.com>
2022-02-24 15:41:36 +0200

    step8-b

---
+++ php-basics-01/step8-b.php
@@ -0,0 +1,8 @@
+<?php
+$arr=array (
+   array('balade', 'mazere3'),
+   array('Fresh', 'Taanayel'),
+   array('Tanoureen', 'Reem'));
+
+    echo "Hey Sir, Please I need 1 pack of {$arr[0][0]} eggs and 3 {$arr[2][0]} Water Pack."
+?>
