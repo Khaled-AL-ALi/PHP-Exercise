5c91849bfc4c88c28ba5402d76e5c0e91dfb80bc
Khaled-AL-ALi <khaled2020pro@gmail.com>
2022-02-24 12:04:50 +0200

    step2

---
+++ php-basics-01/step2.php
@@ -0,0 +1,7 @@
+<?php
+//print and echo are the same but echo is faster and print is very good for expression than echo;
+
+echo "Search on how to enable PHP Errors \n";
+print "I am getting errors but I can't see the errors \n";
+
+?>
