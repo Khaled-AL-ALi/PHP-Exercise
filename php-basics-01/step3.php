6b89de830a5d4f497060dbfcb3fea10a4d287010
Khaled-AL-ALi <khaled2020pro@gmail.com>
2022-02-24 12:38:10 +0200

    step3

---
+++ php-basics-01/step3.php
@@ -0,0 +1,10 @@
+<?php
+$name="khaled";
+$Email= "khaled2020pro@gmail.com";
+$height=2;
+$width=4;
+$area=$height * $width;
+
+echo "My name is " .$name. " and my email is " .$Email;
+print $area;
+?>
