0099c9b5fe097539a81e5afbeebd17315ec6f456
Khaled-AL-ALi <khaled2020pro@gmail.com>
2022-02-28 14:21:41 +0200

    foreach ex-2

---
+++ php-basics-03/foreach-ex-2.php
@@ -0,0 +1,23 @@
+<?php
+$transactions = array(
+    array(
+        "id" => 1,
+        "name"=> "Gaby",
+        "email"=> "gaby@codi.tech"
+    ),
+    array(
+       "id" => 3,
+       "name"=> "Omar",
+       "email"=> "omar@codi.tech"
+    )
+);
+
+foreach($transactions as $value){
+    foreach($value as $user){
+        $id=$value['id'];
+        $name=$value['name'];
+        $email=$value['email'];
+    }
+    echo "id: $id, name: $name, email: $email \n";
+}
+?>
